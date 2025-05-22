de35ede6c04c3659f892857f1bf02a44933b092e
tanakinha2306 <168040313+tanaka2306@users.noreply.github.com>
2025-05-22 19:29:07 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class No {
+    int valor;
+    No esquerdo, direito;
+
+    public No (int valor) {
+        this.valor = valor;
+        esquerdo = direito = null;
+    }
+}
